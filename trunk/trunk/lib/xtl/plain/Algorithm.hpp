0d652f12516f8b12ecc10948cc42700e3ddca9f1
dmitnin@gmail.com <dmitnin@gmail.com@44c3e988-b310-11de-a73b-b36ed9d36964>
2012-04-24 10:50:19 +0000

    *

--- trunk/trunk/lib/xtl/plain/Algorithm.hpp
+++ trunk/trunk/lib/xtl/plain/Algorithm.hpp
@@ -10,12 +10,7 @@ namespace XTL
 {
 namespace PLAIN
 {
-	void Swap(RecordRef & left, RecordRef & right, RecordRef & temp)
-	{
-		temp = left;
-		left = right;
-		right = temp;
-	}
+	void Swap(RecordRef & left, RecordRef & right, RecordRef & temp);
 
 /*
 	QuickSort(RecordArray & array, int low, int high)
@@ -102,34 +97,7 @@ namespace PLAIN
 		QuickSort(array, index, 0, array.Size() - 1, comp);
 	}
 
-	void ArrangeByIndex(RecordArray & array, std::vector<int> & index)
-	{
-		Record temp(array.SharedPrototype());
-
-		for (int i = 0; i < static_cast<int>(array.Size()); ++i)
-		{
-			if (i == index[i] || index[i] == -1)
-			{
-				continue;
-			}
-
-			temp.Assign(array[i]);
-			int to = i;
-			int from = index[to];
-
-			do
-			{
-				array[to].Assign(array[from]);
-				index[to] = -1;
-				to = from;
-				from = index[from];
-			}
-			while (from != i);
-
-			array[to].Assign(temp);
-			index[to] = -1;
-		}
-	}
+	void ArrangeByIndex(RecordArray & array, std::vector<int> & index);
 
 	template <typename RecordComparator_>
 	void Sort(RecordArray & array, const RecordComparator_ & comp)
