f5f8e4acbfd7cce47fbc180b55363e2aa59f97f7
dmitnin@gmail.com <dmitnin@gmail.com@44c3e988-b310-11de-a73b-b36ed9d36964>
2012-06-22 11:56:48 +0000

    *

---
+++ trunk/trunk/lib/xtl/tp/Parser.cpp
@@ -0,0 +1,67 @@
+#include "Parser.hpp"
+
+namespace XTL
+{
+	Parser::Parser(CharSource & charSource)
+		: charSource_(charSource)
+	{
+		;;
+	}
+
+	char Parser::NeedChar() const
+	{
+		if (AtEnd())
+		{
+			throw EndOfFile();
+		}
+
+		return GetChar();
+	}
+
+	void Parser::WaitChar(char c)
+	{
+		while (NotAtEnd())
+		{
+			if (GetChar() == c)
+			{
+				return;
+			}
+
+			Advance();
+		}
+
+		throw EndOfFile();
+	}
+
+	void Parser::SkipChars(char c)
+	{
+		while (NotAtEnd())
+		{
+			if (GetChar() != c)
+			{
+				return;
+			}
+
+			Advance();
+		}
+
+		throw EndOfFile();
+	}
+
+	void Parser::SkipCharClass(const CharClass & charClass)
+	{
+		while (NotAtEnd())
+		{
+			if (!charClass.Contains(GetChar()))
+			{
+				return;
+			}
+
+			Advance();
+		}
+
+		throw EndOfFile();
+	}
+
+}
+
