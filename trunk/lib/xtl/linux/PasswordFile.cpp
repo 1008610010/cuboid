ff6ff3d7dff1371ddf6e5bb5659eecebb14aedd7
dmitnin <dmitnin@gmail.com>
2012-08-13 08:00:39 +0000

    GetGroupId

--- trunk/lib/xtl/linux/PasswordFile.cpp
+++ trunk/lib/xtl/linux/PasswordFile.cpp
@@ -1,5 +1,6 @@
 #include "PasswordFile.hpp"
 
+#include <grp.h>
 #include <pwd.h>
 
 #include <vector>
@@ -10,14 +11,57 @@ namespace XTL
 {
 	namespace
 	{
-		static const unsigned int DEFAULT_BUFFER_SIZE = 128;
+		class MaxSize
+		{
+			public:
+
+				static const long DEFAULT_VALUE = 256;
+
+				static long GetPwName()
+				{
+					return Instance().getpwnam_;
+				}
+				
+				static long GetGrName()
+				{
+					return Instance().getgrnam_;
+				}
+
+			private:
+
+				static const MaxSize & Instance()
+				{
+					static MaxSize instance;
+					return instance;
+				}
+
+				MaxSize()
+					: getpwnam_(DEFAULT_VALUE),
+					  getgrnam_(DEFAULT_VALUE)
+				{
+					getpwnam_ = ::sysconf(_SC_GETPW_R_SIZE_MAX);
+					if (getpwnam_ < 0)
+					{
+						getpwnam_ = DEFAULT_VALUE;
+					}
+
+					getgrnam_ = ::sysconf(_SC_GETGR_R_SIZE_MAX);
+					if (getgrnam_ < 0)
+					{
+						getgrnam_ = DEFAULT_VALUE;
+					}
+				}
+
+				long getpwnam_;
+				long getgrnam_;
+		};
 	}
 
 	uid_t PasswordFile::GetUserId(const std::string & userName)
 	{
 		struct passwd pwd;
 		struct passwd * result;
-		std::vector<char> buffer(DEFAULT_BUFFER_SIZE);
+		std::vector<char> buffer(MaxSize::GetPwName());
 
 		while (true)
 		{
@@ -32,15 +76,45 @@ namespace XTL
 				continue;
 			}
 
-			throw XTL::UnixError();
+			throw UnixError();
 		}
 
 		if (result == 0)
 		{
-			throw UserNotFound();
+			throw NoSuchUser();
 		}
 
 		return result->pw_uid;
+	}
+
+	gid_t PasswordFile::GetGroupId(const std::string & groupName)
+	{
+		struct group grp;
+		struct group * result;
+		std::vector<char> buffer(MaxSize::GetGrName());
+
+		while (true)
+		{
+			if (::getgrnam_r(groupName.c_str(), &grp, &(buffer[0]), buffer.size(), &result) == 0)
+			{
+				break;
+			}
+
+			if (errno == ERANGE)
+			{
+				buffer.resize(buffer.size() * 2);
+				continue;
+			}
+
+			throw UnixError();
+		}
+
+		if (result == 0)
+		{
+			throw NoSuchGroup();
+		}
+
+		return result->gr_gid;
 	}
 }
 
