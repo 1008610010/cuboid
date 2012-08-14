9c4f0bc154fb39bbc1122368cc6d964545f86674
dmitnin@gmail.com <dmitnin@gmail.com@44c3e988-b310-11de-a73b-b36ed9d36964>
2012-08-14 08:32:48 +0000

    SIGSEGV

--- trunk/trunk/lib/xtl/linux/utils/PidFile.cpp
+++ trunk/trunk/lib/xtl/linux/utils/PidFile.cpp
@@ -1,23 +1,7 @@
 #include "PidFile.hpp"
 
-#include <unistd.h>
-
-#include "../../FormatString.hpp"
-#include "../AccessMode.hpp"
-#include "../fs/File.hpp"
-
 namespace XTL
 {
-	std::auto_ptr<PidFile> PidFile::Create(const std::string & fileName)
-	{
-		File f(fileName);
-		f.Create(File::RECREATE_APPEND, AccessMode::DEFAULT_FILE);
-		f.Write(FormatString("%u\n", static_cast<unsigned int>(::getpid())));
-		f.Close();
-
-		return std::auto_ptr<PidFile>(new PidFile(fileName));
-	}
-
 	PidFile::~PidFile() throw()
 	{
 		try
--- trunk/trunk/lib/xtl/linux/utils/PidFile.hpp
+++ trunk/trunk/lib/xtl/linux/utils/PidFile.hpp
@@ -4,6 +4,10 @@
 #include <memory>
 #include <string>
 
+#include "../../FormatString.hpp"
+#include "../AccessMode.hpp"
+#include "../fs/File.hpp"
+
 namespace XTL
 {
 	class PidFile
@@ -11,13 +15,25 @@ namespace XTL
 		public:
 
 			// @throws XTL::UnixError
-			static std::auto_ptr<PidFile> Create(const std::string & fileName);
+			// TODO: Some bullshit happens, when this piece of code moved to PidFile.cpp (SIGSEGV)
+			static std::auto_ptr<PidFile> Create(const std::string & fileName)
+			{
+				File f(fileName);
+				f.Create(File::RECREATE_APPEND, AccessMode::DEFAULT_FILE);
+				f.Write(FormatString("%u\n", static_cast<unsigned int>(::getpid())));
+				f.Close();
+
+				return std::auto_ptr<PidFile>(new PidFile(fileName));
+			}
 
 			~PidFile() throw();
 
 		private:
 
 			explicit PidFile(const std::string & fileName);
+
+			PidFile(const PidFile &);
+			PidFile & operator= (const PidFile &);
 
 			const std::string fileName_;
 	};
