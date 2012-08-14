357314d32a393b62e3741d3fa2776c016e07e129
dmitnin <dmitnin@gmail.com>
2012-08-14 11:58:04 +0000

    Some bullshit with SIGSEGV...

--- trunk/lib/xtl/linux/utils/PidFile.hpp
+++ trunk/lib/xtl/linux/utils/PidFile.hpp
@@ -15,22 +15,11 @@ namespace XTL
 		public:
 
 			// @throws XTL::UnixError
-			// TODO: Some bullshit happens, when this piece of code moved to PidFile.cpp (SIGSEGV)
-			static std::auto_ptr<PidFile> Create(const std::string & fileName)
-			{
-				File f(fileName);
-				f.Create(File::RECREATE_APPEND, AccessMode::DEFAULT_FILE);
-				f.Write(FormatString("%u\n", static_cast<unsigned int>(::getpid())));
-				f.Close();
-
-				return std::auto_ptr<PidFile>(new PidFile(fileName));
-			}
+			explicit PidFile(const std::string & fileName);
 
 			~PidFile() throw();
 
 		private:
-
-			explicit PidFile(const std::string & fileName);
 
 			PidFile(const PidFile &);
 			PidFile & operator= (const PidFile &);
