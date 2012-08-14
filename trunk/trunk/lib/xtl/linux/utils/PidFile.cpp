aad63fa23aaadefba0f7a65b583367188cc73b8b
dmitnin@gmail.com <dmitnin@gmail.com@44c3e988-b310-11de-a73b-b36ed9d36964>
2012-08-14 11:58:04 +0000

    Some bullshit with SIGSEGV...

--- trunk/trunk/lib/xtl/linux/utils/PidFile.cpp
+++ trunk/trunk/lib/xtl/linux/utils/PidFile.cpp
@@ -18,7 +18,10 @@ namespace XTL
 	PidFile::PidFile(const std::string & fileName)
 		: fileName_(fileName)
 	{
-		;;
+		File f(fileName_);
+		f.Create(File::RECREATE_APPEND, AccessMode::DEFAULT_FILE);
+		f.Write(FormatString("%u\n", static_cast<unsigned int>(::getpid())));
+		f.Close();
 	}
 }
 
