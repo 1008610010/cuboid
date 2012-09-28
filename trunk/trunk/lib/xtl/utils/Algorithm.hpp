9cec22c55dfb0c8ad77cb4a0622edbd06614cddc
dmitnin@gmail.com <dmitnin@gmail.com@44c3e988-b310-11de-a73b-b36ed9d36964>
2012-09-28 11:00:37 +0000

    Algorithm CascadeCompare was added.

--- trunk/trunk/lib/xtl/utils/Algorithm.hpp
+++ trunk/trunk/lib/xtl/utils/Algorithm.hpp
@@ -36,7 +36,63 @@ namespace XTL
 
 		return result;
 	}
+
+	template <typename T1>
+	bool CascadeCompare(const T1 & t1left, const T1 & t1right)
+	{
+		return t1left < t1right;
+	}
+
+	template <typename T1, typename T2>
+	bool CascadeCompare(const T1 & t1left, const T1 & t1right,
+	                    const T2 & t2left, const T2 & t2right)
+	{
+		if (CascadeCompare(t1left, t1right))
+		{
+			return true;
+		}
+		else if (CascadeCompare(t1right, t1left))
+		{
+			return false;
+		}
+
+		return CascadeCompare(t2left, t2right);
+	}
+
+	template <typename T1, typename T2, typename T3>
+	bool CascadeCompare(const T1 & t1left, const T1 & t1right,
+	                    const T2 & t2left, const T2 & t2right,
+	                    const T3 & t3left, const T3 & t3right)
+	{
+		if (CascadeCompare(t1left, t1right))
+		{
+			return true;
+		}
+		else if (CascadeCompare(t1right, t1left))
+		{
+			return false;
+		}
+
+		return CascadeCompare(t2left, t2right, t3left, t3right);
+	}
+
+	template <typename T1, typename T2, typename T3, typename T4>
+	bool CascadeCompare(const T1 & t1left, const T1 & t1right,
+	                    const T2 & t2left, const T2 & t2right,
+	                    const T3 & t3left, const T3 & t3right,
+	                    const T4 & t4left, const T4 & t4right)
+	{
+		if (CascadeCompare(t1left, t1right))
+		{
+			return true;
+		}
+		else if (CascadeCompare(t1right, t1left))
+		{
+			return false;
+		}
+
+		return CascadeCompare(t2left, t2right, t3left, t3right, t4left, t4right);
+	}
 }
 
 #endif
-
