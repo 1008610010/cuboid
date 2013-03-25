32bc1e8f0628faeab70f14a73ebc4e2eff743e0e
dmitnin <dmitnin@gmail.com>
2013-03-25 09:48:43 +0000

    *

--- nginx-leveldb-1.3.14/ClearPackage.sh
+++ nginx-leveldb-1.3.14/ClearPackage.sh
@@ -7,9 +7,9 @@ fi
 rm -f doc/html/*
 
 rm -rf CMakeCache.txt CMakeFiles DartTestfile.txt Makefile Testing install_manifest.txt cmake_install.cmake progress.make *.so
-rm build-stamp configure-stamp
+rm -f build-stamp configure-stamp
 rm -rf debian/nginx-leveldb
 rm -rf debian/tmp
-rm debian/nginx-leveldb.debhelper.log
-rm debian/nginx-leveldb.substvars
+rm -f debian/nginx-leveldb.debhelper.log
+rm -f debian/nginx-leveldb.substvars
 
