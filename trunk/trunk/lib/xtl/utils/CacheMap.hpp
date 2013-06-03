5994fdd495c8f64c7f58627456e0d79b5649cf3a
dmitnin@gmail.com <dmitnin@gmail.com@44c3e988-b310-11de-a73b-b36ed9d36964>
2013-06-03 13:03:21 +0000

    Class CachePtrMap was created.

--- trunk/trunk/lib/xtl/utils/CacheMap.hpp
+++ trunk/trunk/lib/xtl/utils/CacheMap.hpp
@@ -92,6 +92,134 @@ namespace XTL
 			Map          map_;
 			List         list_;
 	};
+
+	template <typename Key, typename T>
+	class CachePtrMap
+	{
+		struct ListEntry;
+		struct MapEntry;
+
+		typedef std::list<ListEntry> List;
+		typedef std::map<Key, MapEntry> Map;
+
+		struct ListEntry
+		{
+			typename Map::iterator itrInMap;
+
+			ListEntry(typename Map::iterator itr)
+				: itrInMap(itr) { ;; }
+		};
+
+		struct MapEntry
+		{
+			T * value;
+			typename List::iterator itrInList;
+		};
+
+		public:
+
+			explicit CachePtrMap(unsigned int capacity)
+				: capacity_(capacity),
+				  map_(),
+				  list_()
+			{
+				;;
+			}
+
+			~CachePtrMap() throw()
+			{
+				Clear();
+			}
+
+			unsigned int Capacity() const
+			{
+				return capacity_;
+			}
+
+			unsigned int Size() const
+			{
+				return map_.size();
+			}
+
+			void Clear()
+			{
+				const typename Map::iterator end = map_.end();
+				for (typename Map::iterator itr = map_.begin(); itr != map_.end(); ++itr)
+				{
+					delete itr->second.value;
+				}
+
+				map_.clear();
+				list_.clear();
+			}
+
+			T * Find(const Key & key)
+			{
+				typename Map::iterator mi = map_.find(key);
+
+				if (mi == map_.end())
+				{
+					return 0;
+				}
+
+				Touch(mi);
+
+				return mi->second.value;
+			}
+
+			T * Push(const Key & key, std::auto_ptr<T> value)
+			{
+				typename Map::iterator mi = map_.find(key);
+
+				if (mi == map_.end())
+				{
+					if (map_.size() >= capacity_)
+					{
+						DeleteOldest();
+					}
+
+					MapEntry entry;
+					entry.value = 0;
+					entry.itrInList = list_.end();
+
+					mi = map_.insert(std::make_pair(key, entry)).first;
+				}
+
+				delete mi->second.value;
+				mi->second.value = value.release();
+
+				Touch(mi);
+
+				return mi->second.value;
+			}
+
+		private:
+
+			void DeleteOldest()
+			{
+				typename Map::iterator deleting = list_.back().itrInMap;
+
+				delete deleting->second.value;
+
+				list_.erase(deleting->second.itrInList);
+				map_.erase(deleting);
+			}
+
+			void Touch(typename Map::iterator mi)
+			{
+				if (mi->second.itrInList != list_.end())
+				{
+					list_.erase(mi->second.itrInList);
+				}
+
+				list_.push_front(mi);
+				mi->second.itrInList = list_.begin();
+			}
+
+			unsigned int capacity_;
+			Map          map_;
+			List         list_;
+	};
 }
 
 #endif
