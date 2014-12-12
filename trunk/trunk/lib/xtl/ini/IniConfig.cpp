9c70714ec68ec16506f86fa426674583955d564f
dmitnin@gmail.com <dmitnin@gmail.com@44c3e988-b310-11de-a73b-b36ed9d36964>
2014-12-12 09:34:31 +0000

    Create method XTL::IniConfig::LoadFromFile.

--- trunk/trunk/lib/xtl/ini/IniConfig.cpp
+++ trunk/trunk/lib/xtl/ini/IniConfig.cpp
@@ -1,14 +1,38 @@
 #include "IniConfig.hpp"
 
+#include <vector>
+
+#include "IniConfigParser.hpp"
 #include "../FormatString.hpp"
+#include "../Types.hpp"
 #include "../VariantScalar.hpp"
+#include "../linux/fs/File.hpp"
 
 namespace XTL
 {
+	/********** IniConfig::Error:: **********/
+
+	IniConfig::Error::Error(const std::string & what)
+		: what_(what)
+	{
+		;;
+	}
+
+	IniConfig::Error::~Error() throw()
+	{
+		;;
+	}
+
+	const std::string IniConfig::Error::What() const
+	{
+		return what_;
+	}
+
 	/********** IniConfig::Error::SectionDoesNotExist **********/
 
 	IniConfig::Error::SectionDoesNotExist::SectionDoesNotExist(const std::string & sectionName)
-		: sectionName_(sectionName)
+		: Error(FormatString("Section '%s' does not exist", sectionName)),
+		  sectionName_(sectionName)
 	{
 		;;
 	}
@@ -18,41 +42,32 @@ namespace XTL
 		;;
 	}
 
-	const std::string IniConfig::Error::SectionDoesNotExist::What() const
-	{
-		return FormatString("Section '%s' does not exist", sectionName_);
-	}
-
 	const std::string & IniConfig::Error::SectionDoesNotExist::SectionName() const
 	{
 		return sectionName_;
 	}
 
-	/********** IniConfig::Error::KeyWasNotFound **********/
+	/********** IniConfig::Error::KeyDoesNotExist **********/
 
-	IniConfig::Error::KeyWasNotFound::KeyWasNotFound(const std::string & sectionName, const std::string & key)
-		: sectionName_(sectionName),
+	IniConfig::Error::KeyDoesNotExist::KeyDoesNotExist(const std::string & sectionName, const std::string & key)
+		: Error(FormatString("Section '%s' does not contain key '%s'", sectionName_, key_)),
+		  sectionName_(sectionName),
 		  key_(key)
 	{
 		;;
 	}
 
-	IniConfig::Error::KeyWasNotFound::~KeyWasNotFound() throw()
+	IniConfig::Error::KeyDoesNotExist::~KeyDoesNotExist() throw()
 	{
 		;;
 	}
 
-	const std::string IniConfig::Error::KeyWasNotFound::What() const
-	{
-		return FormatString("Section '%s' does not contain key '%s'", sectionName_, key_);
-	}
-
-	const std::string & IniConfig::Error::KeyWasNotFound::SectionName() const
+	const std::string & IniConfig::Error::KeyDoesNotExist::SectionName() const
 	{
 		return sectionName_;
 	}
 
-	const std::string & IniConfig::Error::KeyWasNotFound::Key() const
+	const std::string & IniConfig::Error::KeyDoesNotExist::Key() const
 	{
 		return key_;
 	}
@@ -76,22 +91,56 @@ namespace XTL
 		pairs_[key] = value;
 	}
 
+	namespace
+	{
+		class AppendTo
+		{
+			public:
+
+				explicit AppendTo(IniConfig::Section & section)
+					: section_(section)
+				{
+					;;
+				}
+
+				void operator() (const std::pair<std::string, VariantPtr> & pair)
+				{
+					section_.Set(pair.first, pair.second);
+				}
+
+			private:
+
+				IniConfig::Section & section_;
+		};
+	}
+
+	void IniConfig::Section::Append(const Section & section)
+	{
+		std::for_each(section.pairs_.begin(), section.pairs_.end(), AppendTo(*this));
+	}
+
 	VariantPtr IniConfig::Section::Get(const std::string & key) const
 	{
 		std::map<std::string, VariantPtr>::const_iterator itr = pairs_.find(key);
 
 		if (itr == pairs_.end())
 		{
-			throw Error::KeyWasNotFound(sectionName_, key);
+			throw Error::KeyDoesNotExist(sectionName_, key);
 		}
 
 		return itr->second;
 	}
 
+	const long long int IniConfig::Section::GetLongLongInt(const std::string & key) const
+	{
+		{
+			return Get(key).ToLongLongInt();
+		}
+	}
+
 	const long long int IniConfig::Section::GetLongLongInt(const std::string & key, const long long int & defaultValue) const
 	{
 		std::map<std::string, VariantPtr>::iterator itr = pairs_.find(key);
-
 		if (itr == pairs_.end())
 		{
 			return defaultValue;
@@ -144,10 +193,17 @@ namespace XTL
 		CreateSection(sectionName).Set(key, value);
 	}
 
+	void IniConfig::Append(const IniConfig & config)
+	{
+		for (AutoPtrMap<std::string, Section>::const_iterator itr = config.sections_.begin(); itr != config.sections_.end(); ++itr)
+		{
+			CreateSection((*itr).first).Append(*((*itr).second));
+		}
+	}
+
 	const IniConfig::Section & IniConfig::GetSectionRequired(const std::string & sectionName) const
 	{
 		Section * section = sections_[sectionName];
-
 		if (section == 0)
 		{
 			throw Error::SectionDoesNotExist(sectionName);
@@ -163,5 +219,29 @@ namespace XTL
 		Section * section = sections_[sectionName];
 
 		return section != 0 ? *section : EMPTY_SECTION;
+	}
+
+	void IniConfig::LoadFromFile(const std::string & filePath)
+	{
+		sections_.Clear();
+
+		File file(filePath);
+		file.Open(File::OPEN_READ_ONLY);
+		FileCloseSentinel fileCloser(file);
+
+		const FileSize fileSize = file.Size();
+		if (fileSize > MAX_INI_CONFIG_SIZE)
+		{
+			throw Error(XTL::FormatString("File '%s' is too big", filePath));
+		}
+
+		std::vector<char> fileContent(fileSize, '\0');
+		if (file.Read(&fileContent[0], fileSize) != fileSize)
+		{
+			throw Error(XTL::FormatString("Read error from file '%s'", filePath));
+		}
+
+		CharSource::ConstCharPtr charSource(&fileContent[0], fileContent.size());
+		IniConfigParser(charSource, *this);
 	}
 }
